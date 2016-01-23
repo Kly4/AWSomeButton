53a1c8a490ca64c0dd2cd9a0bd6d7a9037b35c62
Kly4 <ashablygin@umass.edu>
2016-01-23 01:09:07 -0500

    git stuff

--- buttons.js
+++ buttons.js
@@ -1,9 +1,30 @@
+var Git = require("nodegit");
 
 buttons = {
 
 	'00:25:90:f4:3f:60': {
 		action: function(){
 			console.log('Tide Button ACTIVATED!!!');
+
+
+			//git code 
+			var getMostRecentCommit = function(repository) {
+			  return repository.getBranchCommit("master");
+			};
+
+			var getCommitMessage = function(commit) {
+
+			  console.log("######Recent Commits######");
+			  return commit.message();
+			};
+
+			Git.Repository.open("/Users/Al/Documents/Hackbeanpot_16/AWSomeButton")
+			  .then(getMostRecentCommit)
+			  .then(getCommitMessage)
+			  .then(function(message) {
+			    console.log(message);
+			  });
+
 		}
 	}
 };
