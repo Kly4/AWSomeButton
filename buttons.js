4145f61236de708446586444b849d2b9ff7ccf91
Kly4 <ashablygin@umass.edu>
2016-01-24 13:01:10 -0500

    Added Finish Button

--- buttons.js
+++ buttons.js
@@ -13,13 +13,6 @@ buttons = {
 			console.log('Tide Button ACTIVATED!!!');
 
 
-			// // update repo and get a list of tags
-			// require('simple-git')()
-			//      .log(function(err, log) {
-   //  				console.log(log);
- 		// 		  })
-
-			//#####################################################################
 			//git code 
 			var getMostRecentCommit = function(repository) {
 				return repository.getBranchCommit("master");
@@ -40,6 +33,45 @@ buttons = {
 			 .then(function(message) {
 			   console.log(message);
 			 });
+
+			//################################################
+			//########### Commit and Push Code ###############
+			//################################################
+
+
+
+			// var oid = Commit.create(repo, update_ref, author, committer, message_encoding, message, tree, parent_count, parents);
+
+			// Git.Repository.open("/Users/Al/Documents/Hackbeanpot_16/AWSomeButton")
+			//  .then(getMostRecentCommit)
+			//  .then(getCommitMessage)
+
+			// //  .then(function(oidResult) {
+			// //   oid = oidResult;
+			// //   return nodegit.Reference.nameToId(repo, "HEAD");
+			// // })
+			// // .then(function(head) {
+			// //   return repo.getCommit(head);
+			// // })
+			// // .then(function(parent) {
+			// //   var author = nodegit.Signature.create("Alexey",
+			// //     "kly4@ymail.com");
+			// //   var committer = nodegit.Signature.create("Kly4",
+			// //     "kly4@ymail");
+
+			// //   return repo.createCommit("HEAD", author, committer, "message", oid, [parent]);
+			// // })
+			// // .done(function(commitId) {
+			// //   console.log("New Commit: ", commitId);
+			// // })
+
+
+			//  .then(function(message) {
+			//    console.log(message);
+			//  });
+
+
+
 
 			// // ######################################################################
 
